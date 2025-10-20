4fc1b092ba76ef0e75ef63dc4a5ec9349528ef89
doit-now <hoang.nguyenthe@gmail.com>
2025-10-20 11:34:43 +0700

    update main source by using recursion, to see green4ever

--- src/main/java/com/giaolang/mathutil/core/MathUtility.java
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -15,18 +15,40 @@ public class MathUtility {
     //         xích qua 1 xíu là sang vùng invalid boundary!!!
     //0, xích nhẹ sang trái 1 đơn vị, ko ổn, ko tính đc, -1 sao tính!!!
 
+    //VIẾT HÀM THEO PHONG CÁCH ĐỆ QUY
+    //5! = 1.2.3.4.5 = 4! . 5 = 5 X 4!
+    //4! = 4 X 3!
+    //3! = 3 X 2!
+    //2! = 2 X 1!
+    //1! = 1
+    //            RETURN
+    //KẾT LUẬN: N! = N x (N - 1)! CTHUC DE QUY
+    //            RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN - BÚP BÊ NGA
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về!!!
             throw new IllegalArgumentException("n must be between 0 and 20");
         }
+        if (n == 1 || n == 0)
+            return 1;
 
-        //biến trung gian để tính phép nhân
-        long result = 1;
-
-        for (int i = 1; i <= n; i++) {
-            result *= i;  //result = result * i;  //thuật toán heo đất, ốc bu nhồi thịt, vào đây tiếp chiêu
-        }
-        return result;
+        return n * getFactorial(n - 1);
     }
+    //KIỂM THỬ LẠI CODE ĐÃ TỐI ƯU - REGRESSION TESTING, TEST LẠI THỨ ĐÃ TỪNG TEST
+
+
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về!!!
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//        }
+//
+//        //biến trung gian để tính phép nhân
+//        long result = 1;
+//
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;  //result = result * i;  //thuật toán heo đất, ốc bu nhồi thịt, vào đây tiếp chiêu
+//        }
+//        return result;
+//    }
 }
